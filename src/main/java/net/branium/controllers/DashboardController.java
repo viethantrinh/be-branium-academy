3a3cb4276b25fdb505e7cb9eccf1423216192986
Han Trinh Viet <hntrnn12@gmail.com>
2024-09-25 20:31:25 +0700

    dashboard

---
+++ src/main/java/net/branium/controllers/DashboardController.java
@@ -0,0 +1,79 @@
+package net.branium.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.branium.dtos.base.ApiResponse;
+import net.branium.dtos.user.UserCreateRequest;
+import net.branium.dtos.user.UserResponse;
+import net.branium.dtos.user.UserUpdateRequest;
+import net.branium.services.UserService;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    private final UserService userService;
+
+    // TODO: validate the request body
+    @PostMapping(path = "/users")
+    public ResponseEntity<ApiResponse<UserResponse>> createUser(@RequestBody @Valid UserCreateRequest request) {
+        UserResponse response = userService.createUser(request);
+        var responseBody = ApiResponse.<UserResponse>builder()
+                .message("create user success")
+                .result(response)
+                .build();
+        return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> getUserById(
+            @PathVariable(name = "id") @UUID(message = "ID must be an UUID string") String id) {
+        UserResponse response = userService.getUserById(id);
+        var responseBody = ApiResponse.<UserResponse>builder()
+                .message("get user by id success")
+                .result(response)
+                .build();
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/users")
+    public ResponseEntity<?> listAllUsers() {
+        List<UserResponse> response = userService.getAllUsers();
+        var responseBody = ApiResponse.<List<UserResponse>>builder()
+                .message("get all users success")
+                .result(response)
+                .build();
+        return response.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(responseBody);
+    }
+
+    // TODO: validate the request body
+    @PutMapping(path = "/users/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> updateUserById(@PathVariable(name = "id") String id,
+                                                                    @RequestBody @Valid UserUpdateRequest request) {
+        UserResponse response = userService.updateUser(id, request);
+        var responseBody = ApiResponse.<UserResponse>builder()
+                .message("update user success")
+                .result(response)
+                .build();
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(path = "/users/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> deleteUserById(@PathVariable(name = "id") String id) {
+        userService.deleteUserById(id);
+        var responseBody = ApiResponse.<UserResponse>builder()
+                .message("delete user by id successful")
+                .build();
+        return new ResponseEntity<>(responseBody, HttpStatus.NO_CONTENT);
+    }
+
+}
--- src/main/java/net/branium/controllers/UserController.java
+++ src/main/java/net/branium/controllers/UserController.java
@@ -24,62 +24,6 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    // TODO: validate the request body
-    @PostMapping
-    public ResponseEntity<ApiResponse<UserResponse>> createUser(@RequestBody @Valid UserCreateRequest request) {
-        UserResponse response = userService.createUser(request);
-        var responseBody = ApiResponse.<UserResponse>builder()
-                .message("create user success")
-                .result(response)
-                .build();
-        return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
-    }
-
-    @GetMapping(path = "/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> getUserById(
-            @PathVariable(name = "id") @UUID(message = "ID must be an UUID string") String id) {
-        UserResponse response = userService.getUserById(id);
-        var responseBody = ApiResponse.<UserResponse>builder()
-                .message("get user by id success")
-                .result(response)
-                .build();
-        return new ResponseEntity<>(responseBody, HttpStatus.OK);
-    }
-
-    @GetMapping
-    public ResponseEntity<?> listAllUsers() {
-        List<UserResponse> response = userService.getAllUsers();
-        var responseBody = ApiResponse.<List<UserResponse>>builder()
-                .message("get all users success")
-                .result(response)
-                .build();
-        return response.isEmpty()
-                ? ResponseEntity.noContent().build()
-                : ResponseEntity.ok(responseBody);
-    }
-
-    // TODO: validate the request body
-    @PutMapping(path = "/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> updateUserById(@PathVariable(name = "id") String id,
-                                                                    @RequestBody @Valid UserUpdateRequest request) {
-        UserResponse response = userService.updateUser(id, request);
-        var responseBody = ApiResponse.<UserResponse>builder()
-                .message("update user success")
-                .result(response)
-                .build();
-        return new ResponseEntity<>(responseBody, HttpStatus.OK);
-    }
-
-
-    @DeleteMapping(path = "/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> deleteUserById(@PathVariable(name = "id") String id) {
-        userService.deleteUserById(id);
-        var responseBody = ApiResponse.<UserResponse>builder()
-                .message("delete user by id successful")
-                .build();
-        return new ResponseEntity<>(responseBody, HttpStatus.NO_CONTENT);
-    }
-
     @GetMapping(path = "/info")
     public ResponseEntity<ApiResponse<StudentResponse>> getStudentInfo() {
         StudentResponse response = userService.getStudentInfo();
